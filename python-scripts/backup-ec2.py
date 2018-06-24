a3dcb67341af6b38bfc3d43062b014ee2ec3a5f7
evelyn araujo <evelin.araujo@metalab.csun.edu>
2018-06-23 19:03:51 -0700

    updated ec2 backup lambda

--- python-scripts/backup-ec2.py
+++ python-scripts/backup-ec2.py
@@ -4,18 +4,33 @@ import boto3
 import logging
 from datetime import datetime, timedelta
 
-# Describe the ec2 instance and grab the ebs volume
-client = boto3.resource('ec2')
-# variable for ec2 instance
-ec2_id = ['']
 
-for id in ec2_id:
-    response = client.Describe_instances()
-    for i in response["Instances"]
-    print(instance)
-    print(instance['root-device-nmaae'])
-    print(instance['block-device-mapping.volume-id'])
-
-# date=
-# for id in ec2_id:
-#     snapshot = "{0}-{1}".format(id, date)
+def describe_ec2(event, context):
+    # Describe the ec2 instance and grab the ebs volume
+    client = boto3.client('ec2')
+    # variable for ec2 instance
+    ec2_id = ['']
+    
+    for id in ec2_id:
+        response = client.describe_instances()
+        for reservation in response['Reservations']:
+            for instance in reservation['Instances']:
+                #print(instance)
+                volumes=instance['BlockDeviceMappings']
+                for volume in volumes:
+                        volume=volume['Ebs']['VolumeId']
+                        print("Volume ID for instance ID %s is %s " % (id, volume))
+                        date = (datetime.now()).strftime("%F")
+                        client.create_snapshot(
+                            Description = "Snapshot for " + id, 
+                            VolumeId=volume,
+                            TagSpecifications=[
+                                {
+                                    'ResourceType': "instance"
+                                    'Tags': [
+                                        'Key': 'Name',
+                                        'Value': Name="{0}-{1}".format(id, date),
+                                        ]
+                                }
+                                ]
+                                )
