11517d52d8a6875bedd38b807ae41d18aca7fd76
evelyn araujo <evelin.araujo@metalab.csun.edu>
2018-06-23 21:25:16 -0700

    added tags and fixed logging info

--- python-scripts/backup-ec2.py
+++ python-scripts/backup-ec2.py
@@ -12,21 +12,25 @@ def describe_ec2(event, context):
 
     client = boto3.client('ec2')
     # variable for ec2 instance
+    
     ec2_id = ['']
+    
     # Describe the ec2 instance and grab the ebs volume
     for id in ec2_id:
         response = client.describe_instances()
         for reservation in response['Reservations']:
             for instance in reservation['Instances']:
                 #print(instance)
+                
                 #Grab volume from instance
-                volumes=instance['BlockDevice Mappings']
+                volumes=instance['BlockDeviceMappings']
                 for volume in volumes:
                         volume=volume['Ebs']['VolumeId']
                         print("Volume ID for instance ID %s is %s " % (id, volume))
                         date = (datetime.now()).strftime("%F")
                         snapshot_name="{0}-{1}".format('evelin-test', date)
                         #Create snapshot from volume
+                        
                         snapshot=client.create_snapshot(
                             Description = "Snapshot for " + id, 
                             VolumeId=volume,
@@ -35,12 +39,24 @@ def describe_ec2(event, context):
                                     'ResourceType': "snapshot",
                                     'Tags': [
                                         {
-                                            'Key': 'Name',
-                                            'Value': snapshot_name
+                                            'Key': 'name',
+                                            'Value': snapshot_name,
+                                            'Key': 'application',
+                                            'Value': 'core_infrastructure',
+                                            'Key': 'department',
+                                            'Value': 'infrastructures',
+                                            'Key': 'division',
+                                            'Value': 'technology',
+                                            'Key': 'environment',
+                                            'Value': 'development' ,
+                                            'Key': 'role',
+                                            'Value': 'backup'
+
                                         },
                                     ]
                                 }
                             ]
                         )
-                        logging.info(snapshot + "is being created :) ")
-                        logging.error("Womp need to catch some errors")
+                        logging.info("%s is being created :) " % snapshot_name)
+
+#Make sure to adjust the timeout on the function. Default is 3 seconds, needs at least 10 seconds 
