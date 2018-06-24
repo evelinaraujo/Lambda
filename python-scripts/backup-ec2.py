23c6c37cbe32a56e20c9a80a80b404fa971e367c
evelyn araujo <evelin.araujo@metalab.csun.edu>
2018-06-23 20:14:16 -0700

    adding changes

--- python-scripts/backup-ec2.py
+++ python-scripts/backup-ec2.py
@@ -9,22 +9,24 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def describe_ec2(event, context):
-    # Describe the ec2 instance and grab the ebs volume
+
     client = boto3.client('ec2')
     # variable for ec2 instance
-    ec2_id = ['i-05167199d5ca187dc']
-    
+    ec2_id = ['']
+    # Describe the ec2 instance and grab the ebs volume
     for id in ec2_id:
         response = client.describe_instances()
         for reservation in response['Reservations']:
             for instance in reservation['Instances']:
                 #print(instance)
-                volumes=instance['BlockDeviceMappings']
+                #Grab volume from instance
+                volumes=instance['BlockDevice Mappings']
                 for volume in volumes:
                         volume=volume['Ebs']['VolumeId']
                         print("Volume ID for instance ID %s is %s " % (id, volume))
                         date = (datetime.now()).strftime("%F")
                         snapshot_name="{0}-{1}".format('evelin-test', date)
+                        #Create snapshot from volume
                         snapshot=client.create_snapshot(
                             Description = "Snapshot for " + id, 
                             VolumeId=volume,
